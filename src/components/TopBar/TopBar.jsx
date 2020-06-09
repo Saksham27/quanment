83c788b380aaf3f41e568f5c66553069aa8ea634
Saksham27 <geekysaksham27@gmail.com>
2020-06-09 19:36:39 +0530

    Added : Topbar component

---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './TopBar.scss'
+
+function TopBar(props) {
+    return (
+        <div class='container'>
+            <div class='quantment'>Quantment</div>
+            <div/>
+            <div class="history">History</div>
+        </div>
+    )
+}
+
+
+export default TopBar
+
