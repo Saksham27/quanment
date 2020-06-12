eecdded0ac2f0df000f7274b9cb0e4d0d414ccf7
Saksham27 <geekysaksham27@gmail.com>
2020-06-12 20:58:16 +0530

    Updates

---
+++ precache-manifest.50f06aafb8e98a0be3fb81c77a689aa9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede7e164a93c692b81f7ea32579a8687",
+    "url": "/quanment/index.html"
+  },
+  {
+    "revision": "4f9dd47924aa86e383b7",
+    "url": "/quanment/static/css/main.3fbbaecd.chunk.css"
+  },
+  {
+    "revision": "83741fc78abfef5678aa",
+    "url": "/quanment/static/js/2.ab1d4d3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quanment/static/js/2.ab1d4d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9dd47924aa86e383b7",
+    "url": "/quanment/static/js/main.9cdab46f.chunk.js"
+  },
+  {
+    "revision": "7520cfe659506c0533c0",
+    "url": "/quanment/static/js/runtime-main.5f36e0d9.js"
+  },
+  {
+    "revision": "c824fced90cfec6bef206373cc717f68",
+    "url": "/quanment/static/media/beaker.c824fced.svg"
+  },
+  {
+    "revision": "741ab4aa6e6e9c35ab4c7dbab66fe7e4",
+    "url": "/quanment/static/media/beaker_inactive.741ab4aa.svg"
+  },
+  {
+    "revision": "73b39610a0c24d67f332e35facc45d3e",
+    "url": "/quanment/static/media/hot.73b39610.svg"
+  },
+  {
+    "revision": "68ef5287aba62062ac9ae1545a2c49af",
+    "url": "/quanment/static/media/hot_inactive.68ef5287.svg"
+  },
+  {
+    "revision": "55317149a4974665290f79342a16140e",
+    "url": "/quanment/static/media/scale.55317149.svg"
+  },
+  {
+    "revision": "6bae66d07be0c351824687ce9a437732",
+    "url": "/quanment/static/media/scale_inactive.6bae66d0.svg"
+  }
+]);
